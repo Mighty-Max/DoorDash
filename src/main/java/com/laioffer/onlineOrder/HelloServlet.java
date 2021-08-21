19392311511fc572c0a382e4486bd118466a8f1a
Mighty-Max <jiacheng0402@gmail.com>
2021-08-21 17:23:31 -0500

    initialized project first commit

---
+++ src/main/java/com/laioffer/onlineOrder/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.laioffer.onlineOrder;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+
+        // Hello
+        String customer = request.getParameter("customer");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello " + customer + "</h1>");
+        out.println("</body></html>");
+        out.println("wwwwwww");
+    }
+
+    public void destroy() {
+    }
+}
